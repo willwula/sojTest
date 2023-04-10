14a6179ab4c7f50ac5a3f31259bdca2acd2e65ff
willwula <wfh318@gmail.com>
2023-04-10 16:11:41 +0800

    feat:RegisterController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+
+Route::prefix('user')->group(function() {
+    Route::post('register', [RegisterController::class, 'register']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

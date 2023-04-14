ec0548a53bf5e5bbf32ea9ac618d0f74976799b0
willwula <wfh318@gmail.com>
2023-04-14 14:59:33 +0800

    feat:Create a Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
-    //
+
+    public function store(Request $request) {
+        $user = Auth::user(); //現在登入的人
+        $validated = $this->validate($request,[
+            'name' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+        ]);
+        return $user->books()->create($validated);
+    }
 }
--- database/migrations/2023_04_12_055713_create_books_table.php
+++ database/migrations/2023_04_12_055713_create_books_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id');  //誰擁有這本書
             $table->string('name');
             $table->string('author');
             $table->timestamps();
